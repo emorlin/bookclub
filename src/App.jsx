9027d21899b0c31b9973724812c6a72133c569a5
emorlin <emorlin@gmail.com>
2025-09-03 11:35:33 +0200

    removed all old navigation logic

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import StartpageHero from "./components/StartpageHero";
 import BookList from "./components/BookList";
 import Book from "./components/Book";
 import ErrorBoundary from "./components/ErrorBoundary";
-import navigationValues from "./navigation/navigationValues";
-import navigationContext from "./navigation/navigationContext";
+
 import ComponentPicker from "./components/ComponentPicker";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -21,29 +20,23 @@ function App() {
         setSelectedBook(book);
     }, []);
 
-    const navigate = useCallback((navTo) => setNav(navTo), []);
-    const [nav, setNav] = useState({ current: navigationValues.home, navigate });
-
     return (
         <>
-            <navigationContext.Provider value={nav}>
-                <ErrorBoundary fallback="Error">
-                    <Header></Header>
-                    <main>
-                        {selectedBook ? (
-                            <Book book={selectedBook} />
-                        ) : (
-                            <>
-                                <StartpageHero></StartpageHero>
-                                <BookList onSelectedBook={handleSelectedBook}></BookList>
-                            </>
-                        )}
-                        {/*  <ComponentPicker currentLocation={nav.current}></ComponentPicker>*/}
-                    </main>
-
-                    <Footer></Footer>
-                </ErrorBoundary>
-            </navigationContext.Provider>
+            <ErrorBoundary fallback="Något har gått fruktansvärt fel.">
+                <Header></Header>
+                <main>
+                    {selectedBook ? (
+                        <Book book={selectedBook} />
+                    ) : (
+                        <>
+                            <StartpageHero></StartpageHero>
+                            <BookList onSelectedBook={handleSelectedBook}></BookList>
+                        </>
+                    )}
+                    {/*  <ComponentPicker currentLocation={nav.current}></ComponentPicker>*/}
+                </main>
+                <Footer></Footer>
+            </ErrorBoundary>
         </>
     );
 }
--- src/navigation/navigationValues.js
+++
@@ -1,7 +0,0 @@
-const navigationValues = {
-    home: "Startsida",
-    book: "Bok",
-    addBook: "Lägg till bok",
-    about: "Om",
-};
-export default navigationValues;
