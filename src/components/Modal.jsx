dafe99ecd5cdde9730f85b3e8a5287332d50879c
emorlin <emorlin@gmail.com>
2025-09-04 15:53:53 +0200

    Added missing keys

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -176,7 +176,7 @@ export default function Modal() {
                                     </div>
 
                                     {readers.map(({ displayName, gradeField }) => (
-                                        <>
+                                        <div key={displayName}>
                                             <label
                                                 htmlFor={displayName}
                                                 className="block text-sm/6 font-medium text-gray-900 mt-4">
@@ -194,7 +194,7 @@ export default function Modal() {
                                                     <option value="5">5</option>
                                                 </select>
                                             </div>
-                                        </>
+                                        </div>
                                     ))}
 
                                     <label
@@ -239,7 +239,7 @@ export default function Modal() {
                                             name="country"
                                             type="text"
                                             autoComplete="off"
-                                            value=""
+                                            defaultValue=""
                                             className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                             placeholder="Germany"
                                         />
@@ -255,7 +255,7 @@ export default function Modal() {
                                             name="bookLink"
                                             type="text"
                                             autoComplete="off"
-                                            value=""
+                                            defaultValue=""
                                             className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                             placeholder="https://goodreads..."
                                         />
@@ -271,7 +271,7 @@ export default function Modal() {
                                             name="authorLink"
                                             type="text"
                                             autoComplete="off"
-                                            value=""
+                                            defaultValue=""
                                             className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                             placeholder="https://goodreads..."
                                         />
