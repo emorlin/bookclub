6666f52cddb67d1b5c17e97e317c7f68d3405c47
emorlin <emorlin@gmail.com>
2025-09-07 16:09:53 +0200

    Added PW-check

--- api/contentful/createBook.js
+++ api/contentful/createBook.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
     console.log("providedBody:", body?.secret);
 
     if (!expected || !provided || !timingSafeEq(provided, expected)) {
-        //    return res.status(401).json({ error: "Unauthorized" });
+        return res.status(401).json({ error: "Unauthorized" });
     }
 
     if (req.method !== "POST") return res.status(405).json({ error: "Method Not Allowed" });
