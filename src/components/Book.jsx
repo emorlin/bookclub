9c0b6b0e69411a39845cd0c1a27439fcee458641
emorlin <emorlin@gmail.com>
2025-10-02 15:58:39 +0200

    Cosmetics

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -8,7 +8,7 @@ import { useModal } from "../hooks/useModal";
 import { HashLink } from "react-router-hash-link";
 import { translateLanguage } from "../utils/languageLookup";
 import { useSelectedBook } from "../hooks/useSelectedBook";
-import { ExternalLink } from "lucide-react";
+import { ExternalLink, ArrowLeft } from "lucide-react";
 
 const Book = () => {
     const selectedBook = useSelectedBook();
@@ -39,19 +39,23 @@ const Book = () => {
     const languageLabel = useMemo(() => translateLanguage(langName), [langName]);
 
     return (
-        <div className="overflow-hidden bg-white dark:bg-gray-300 py-22 sm:py-26">
+        <div className="overflow-hidden bg-white dark:bg-gray-900 py-22 sm:py-26">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <HashLink
                     smooth
                     to="/#bookstable"
-                    className="relative -top-6 inline-block text-md font-semibold leading-6 text-pretty">
-                    ← <span className="underline">Alla lästa böcker</span>
+                    className="relative -top-6 inline-block text-md font-semibold leading-6 text-pretty dark:text-white">
+                    <ArrowLeft
+                        size={18}
+                        className="inline-block -mt-1 "
+                    />{" "}
+                    <span className="underline">Alla lästa böcker</span>
                 </HashLink>
-                <article className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                <article className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 dark:text-white">
                     <div className="lg:pr-8">
                         <div className="lg:max-w-lg">
                             {bookTitle && (
-                                <h2 className="text-4xl font-semibold tracking-tight text-pretty dark:text-gray-900 sm:text-5xl">
+                                <h2 className="text-4xl font-semibold tracking-tight text-pretty dark:text-white sm:text-5xl">
                                     {bookLink ? (
                                         <a
                                             className="underline underline-offset-2 decoration-2"
@@ -109,24 +113,24 @@ const Book = () => {
                                 onClick={() => openModal({ isbn })}
                                 aria-controls="book-modal"
                                 aria-expanded={isOpen}
-                                className="mt-8 px-2 py-1 rounded border border-black text-black text-sm cursor-pointer">
+                                className="mt-8 px-2 py-1 rounded border border-black dark:border-white text-black dark:text-white text-sm cursor-pointer">
                                 Uppdatera bokdata
                             </button>
                             <div className="mt-8">
-                                <h3 className="text-base/7 font-semibold dark:text-gray-900">Bokinformation:</h3>
+                                <h3 className="text-base/7 font-semibold dark:text-white">Bokinformation:</h3>
                                 <div className="mt-2 border-t dark:border-gray-100">
                                     <dl className="divide-y divide-gray-100">
                                         {isbn && (
                                             <div className="px-1 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                                <dt className="text-sm/6 font-medium dark:text-gray-900">ISBN</dt>
+                                                <dt className="text-sm/6 font-medium dark:text-white">ISBN</dt>
                                                 <dd className="mt-1 text-sm/6 dark:text-gray-700 sm:col-span-2 sm:mt-0">
                                                     {bookLink ? (
                                                         <a
-                                                            className="text-blue-600"
+                                                            className="dark:text-white"
                                                             href={bookLink}
                                                             target="_blank"
                                                             rel="noreferrer">
-                                                            <span className="underline">{isbn}</span>
+                                                            <span className="underline ">{isbn}</span>
 
                                                             <ExternalLink
                                                                 size={14}
@@ -178,8 +182,8 @@ const Book = () => {
 function InfoRow({ label, children }) {
     return (
         <div className="px-1 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm/6 font-medium dark:text-gray-900">{label}</dt>
-            <dd className="mt-1 text-sm/6 dark:text-gray-700 sm:col-span-2 sm:mt-0">{children}</dd>
+            <dt className="text-sm/6 font-medium dark:text-white">{label}</dt>
+            <dd className="mt-1 text-sm/6 dark:text-gray-700 sm:col-span-2 sm:mt-0 dark:text-white">{children}</dd>
         </div>
     );
 }
